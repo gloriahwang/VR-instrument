24bc2eebc04157f49da40747cfcbe5c38540cc42
Jiwoong Han <jihan2311@gmail.com>
2017-11-06 17:29:54 -0800

    make drum button

---
+++ Vr Inst/Assets/Scripts/Drum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drum : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 
+	}
+
+	static void activate () {
+		this.setActive (false);
+	}
+
+	static void kill () {
+		
+	}
+}
---
+++ Vr Inst/Assets/Scripts/Hand.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+	public OVRInput.Controller controller;
+
+	private float indexTriggerState = 0;
+	private float handTriggerState = 0;
+	private float oldIndexTriggerState = 0;
+
+	private bool drum_activated = false;
+	public Drum drum;
+
+
+	// Update is called once per frame
+	void Update () {
+		oldIndexTriggerState = indexTriggerState;
+		indexTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+		handTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
+
+		if (indexTriggerState > 0.9f && oldIndexTriggerState < 0.9f) {
+			select ();
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		
+
+	}
+
+	void select () {
+
+		RaycastHit hit;
+		Vector3 origin = particleSystem.transform.position;
+		Vector3 direction = particleSystem.transform.right;
+
+		if (Physics.Raycast (origin, direction, out hit, 100f)) {
+			Debug.DrawRay (origin, direction);
+			GameObject hitObject = hit.collider.gameObject;
+			if (hitObject.CompareTag ("drum_button")) {
+				if (!drum_activated) {
+					drum_activated = true;
+					hitObject.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+					Drum.activate ();
+				} else {
+					drum_activated = false;
+					hitObject.gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+					Drum.kill ();
+				}
+			}
+		}
+
+
+
+	}
+}
